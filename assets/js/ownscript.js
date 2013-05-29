2ec4ec66720653c4135eb8322f6a520a4c86cd6c
avanthay <dave@avanthay.ch>
2013-05-29 02:11:37 +0200

    datenfelder für formular gesetzt

---
+++ assets/js/ownscript.js
@@ -0,0 +1,26 @@
+﻿$("#button1").on("click", function () {
+    $(".formfields").css("border-color", "#5BB75B");
+    $(".formfields").css("box-shadow", "#5BB75B 0px 0px 3px 0px");
+});
+
+$("#btn-group").tooltip();
+
+$("#button2").click( function () {
+    $(".formfields").css("border-color", "#F9A329");
+    $(".formfields").css("box-shadow", "#F9A329 0px 0px 3px 0px");
+});
+
+$("#button3").on("click", function (event) {
+    $(".formfields").css("border-color", "#D74B46");
+    $(".formfields").css("box-shadow", "#D74B46 0px 0px 3px 0px");
+});
+
+
+$(function () {
+    $("#datepicker").datepicker({
+        minDate: 0,
+        showWeek: true,
+        firstDay: 1,
+        dateFormat: "dd.mm.yy"
+    });
+});
