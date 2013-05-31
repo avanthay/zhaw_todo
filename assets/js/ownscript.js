db042c41cb78ffa628fd83c5e9df7cd7b6beaca2
avanthay <dave@avanthay.ch>
2013-05-31 12:29:24 +0200

    taskoverview open / done erstellt

--- assets/js/ownscript.js
+++ assets/js/ownscript.js
@@ -1,47 +1,77 @@
 ﻿$("#button1").on("click", function () {
-    $(".formfields").css("border-color", "#5BB75B");
-    $(".formfields").css("box-shadow", "#5BB75B 0px 0px 3px 0px");
+    $(".formfields").css("border-color", "#D74B46");
+    $(".formfields").css("box-shadow", "#D74B46 0px 0px 3px 0px");
+    prio = 1;
 });
 
 $("#button2").on("click", function () {
     $(".formfields").css("border-color", "#F9A329");
     $(".formfields").css("box-shadow", "#F9A329 0px 0px 3px 0px");
+    prio = 2;
 });
 
-$("#button3").on("click", function (event) {
-    $(".formfields").css("border-color", "#D74B46");
-    $(".formfields").css("box-shadow", "#D74B46 0px 0px 3px 0px");
+$("#button3").on("click", function () {
+    $(".formfields").css("border-color", "#5BB75B");
+    $(".formfields").css("box-shadow", "#5BB75B 0px 0px 3px 0px");
+    prio = 3;
 });
 
-$("#btn-group").tooltip();
-
-
-$(function () {
+$(function onload () {
+    var standardDate = $.datepicker.formatDate("dd.mm.yy", new Date());
+    document.getElementById('datepicker').value = standardDate;
     $("#datepicker").datepicker({
         minDate: 0,
         showWeek: true,
         firstDay: 1,
         dateFormat: "dd.mm.yy",
     });
+    $('#button3').click();
 });
 
-var hoi = 0;
+$('#extend-button').click(function () {
+    var eyeIcon = document.getElementById("eye-icon");
+    if (eyeIcon.getAttribute("class") == "icon-eye-open") {
+        eyeIcon.removeAttribute("class", "icon-eye-open");
+        eyeIcon.setAttribute("class", "icon-eye-close");
+    } else {
+        eyeIcon.removeAttribute("class", "icon-eye-close");
+        eyeIcon.setAttribute("class", "icon-eye-open");
+    }
+});
 
-$("#validatebutton").click(function () {
+$('#extend-button').tooltip();
+
+var hoi = null;
+var taskArray = new Array();
+
+
+$("#validateButton").click(function () {
     hoi++;
-    var taskID = document.createElement("div");
-    taskID.setAttribute("class", "row");
+    var taskID = document.createElement("p");
+    taskID.setAttribute("class", "formfields");
     taskID.setAttribute("id", "row" + hoi);
-    var wo = document.getElementById("taskList");
-    wo.appendChild(taskID);
+    document.getElementById("taskList").appendChild(taskID);
     createDate();
     createTask();
 });
 
-function createPrio() {
-    //get prio
+$('#about').click(function () {
+    registerTask();
+});
 
+function registerTask() {
+    var dateValue = document.getElementById("datepicker");
+    var taskValue = document.getElementById("taskName");
+    var task = {
+        id: hoi,
+        prio: prio,
+        date: dateValue.value,
+        name: taskValue.value,
+        log: new Date().getTime()
+    }
+    taskArray.push(task); 
 };
+
 
 function createTask() {
     //task auslesen
